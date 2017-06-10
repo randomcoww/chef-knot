a7cff5e979873be13379451f4b54c98441eb1304
randomcoww <etherblimp@gmail.com>
2017-06-10 00:07:21 -0700

    config format

--- libraries/resource/config.rb
+++ libraries/resource/config.rb
@@ -7,10 +7,16 @@ class ChefKnot
       allowed_actions :create, :delete
 
       property :config, Hash
-      property :content, [String,NilClass], default: lazy { config.to_yaml }
+      property :content, [String,NilClass], default: lazy { to_conf }
       property :path, String, desired_state: false,
                               default: lazy { KnotHelper::CONFIG_PATH }
 
+      private
+
+      def to_conf
+        ## config is very yaml like but --- separator does not work
+        config.to_hash.to_yaml.lines.drop(1).join('')
+      end
     end
   end
 end
